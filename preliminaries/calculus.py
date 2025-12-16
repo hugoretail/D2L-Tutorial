0f0229429ff45b33db13d7fd3543fb95eea83af7
Retail Hugo <hugo.retail@etu.u-bordeaux.fr>
2025-12-16 09:09:49 +0100

    calculus done

--- preliminaries/calculus.py
+++ preliminaries/calculus.py
@@ -51,5 +51,6 @@ def f(x):
 
 x = np.arange(0, 3, 0.1)
 plot(x, [f(x), 2 * x - 3], 'x', 'f(x)', legend=['f(x)', 'Tangent line (x=1)'])
+plot(x, [f(x), 2 * x - 3], 'x', 'f(x)', legend=['f(x)', 'Tangent line (x=1)'])
 # plt.show()
 
