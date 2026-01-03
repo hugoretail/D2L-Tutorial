051c0d67179f3223c9ce75bb11c9c2bf6a7f0208
Retail Hugo <hugo.retail@etu.u-bordeaux.fr>
2026-01-03 17:59:57 +0100

    Linalg netwks regression DONE

--- linear_neural_networks_for_reg/linear_neural_networks_for_reg.md
+++ linear_neural_networks_for_reg/linear_neural_networks_for_reg.md
@@ -1,3 +1,3 @@
 # Linear neural networks for regression
 
-Resume at: 3.1.1.3 Analytic Solution
+Resume at: 3.2. Object-Oriented Design for Implementation
--- linear_neural_networks_for_reg/linear_regression.py
+++ linear_neural_networks_for_reg/linear_regression.py
@@ -4,4 +4,33 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 
+n = 10000
+a = torch.ones(n)
+b = torch.ones(n)
+print(a)
 
+c = torch.zeros(n)
+t = time.time()
+for i in range(n):
+  c[i] = a[i] + b[i]
+# print(f'{time.time() - t:5f} sec')
+
+t = time.time()
+d = a + b
+# print(f"{time.time() - t:5f} sec")
+
+def normal(x, mu, sigma):
+  p= 1/math.sqrt(2*math.pi * sigma**2)
+  return p * np.exp(-0.5 * (x-mu) ** 2 / sigma**2 )
+
+# Use NumPy again for visualization
+x = np.arange(-7, 7, 0.01)
+# Mean and standard deviation pairs
+params = [(0, 1), (0, 2), (3, 1)]
+for mu, sigma in params:
+  plt.plot(x, normal(x, mu, sigma), label=f'mean {mu}, std {sigma}')
+plt.xlabel('x')
+plt.ylabel('p(x)')
+plt.legend()
+plt.gcf().set_size_inches(4.5, 2.5)
+plt.show()
