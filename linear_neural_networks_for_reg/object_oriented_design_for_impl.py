d613208df597b74023f8c2cb90e9583ee5e431e2
Retail Hugo <hugo.retail@etu.u-bordeaux.fr>
2026-01-05 14:23:42 +0100

    3.2 Done

--- linear_neural_networks_for_reg/object_oriented_design_for_impl.py
+++ linear_neural_networks_for_reg/object_oriented_design_for_impl.py
@@ -121,3 +121,83 @@ board = ProgressBoard('x')
 for x in np.arange(0,10,0.1):
   board.draw(x, np.sin(x), 'sin', every_n=2)
   board.draw(x, np.cos(x), 'cos', every_n=10)
+
+class Module(nn.Module, HyperParameters):
+  def __init__(self, plot_train_per_epoch=2, plot_valid_per_epoch=1):
+    super().__init__()
+    self.save_hyperparameters()
+    self.board = ProgressBoard()
+    
+  def loss(self, y_hat, y):
+    raise NotImplementedError
+  
+  def forward(self, X):
+    assert hasattr(self, 'net'), 'Neural network is defined'
+    
+    def plot(self, key, value, train):
+      """Plot a point in animation."""
+      assert hasattr(self, 'trainer'), 'Trainer is not inited'
+      self.board.xlabel = 'epoch'
+      if train:
+        x = float(self.trainer.train_batch_idx) / float(self.trainer.num_train_batches)
+        n = float(self.trainer.num_train_batches) / float(self.plot_train_per_epoch)
+      else:
+        x = float(self.trainer.epoch) + 1.0
+        n = float(self.trainer.num_val_batches) / float(self.plot_valid_per_epoch)
+      self.board.draw(x, value.detach().cpu().numpy(),
+              ('train_' if train else 'val_') + key,
+              every_n=int(n))
+    
+    def training_step(self, batch):
+      l = self.loss(self(*batch[-1]), batch[-1])
+      self.plot('loss', l, train=True)
+      
+    def validation_step(self, batch):
+      l = self.loss(self(*batch[:-1]),batch[-1])
+      self.plot('loss',l,train=False)
+    
+    def configure_optimizers(self):
+      raise NotImplementedError
+    
+class DataModule(HyperParameters):
+  def __init__(self, root='../data', num_workers=4):
+    self.save_hyperparameters()
+    
+  def get_dataloader(self,train):
+    raise NotImplementedError
+  
+  def train_dataloader(self):
+    return self.get_dataloader(train=True)
+  
+  def val_dataloader(self):
+    return self.get_dataloader(train=False)
+
+class Trainer(nn.Module, HyperParameters):
+  """The base class for training models with data."""
+  def __init__(self, max_epochs, num_gpus=0, gradient_clip_val=0):
+    self.save_hyperparameters()
+    assert num_gpus == 0, 'No GPU support yet'
+    
+    def prepare_data(self, data):
+        self.train_dataloader = data.train_dataloader()
+        self.val_dataloader = data.val_dataloader()
+        self.num_train_batches = len(self.train_dataloader)
+        self.num_val_batches = len(self.val_dataloader) if self.val_dataloader is not None else 0
+    
+  def prepare_model(self, model):
+    model.trainer = self
+    model.board.xlim = [0, self.max_epochs]
+    self.model = model
+    
+    def fit(self, model, data):
+      self.prepare_data(data)
+      self.prepare_model(model)
+      self.optim = model.configure_optimizers()
+      for epoch in range(self.max_epochs):
+        self.epoch = epoch
+        self.train_batch_idx = 0
+        self.val_batch_idx = 0
+        self.fit_epoch()
+
+    def fit_epoch(self):
+      raise NotImplementedError
