78f3893ffc21cca59833f68240092cd4f66ecf44
Retail Hugo <hugo.retail@etu.u-bordeaux.fr>
2026-02-11 14:23:17 +0100

    4.3.3 done:exercises todo

--- linear_neural_networks_for_classification/the_base_classification_model.py
+++ linear_neural_networks_for_classification/the_base_classification_model.py
@@ -7,12 +7,13 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import d2l
+import numpy as np
+import gluon #type: ignore
 
 # Add the linear_neural_networks_for_reg directory to sys.path for import
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../linear_neural_networks_for_reg')))
-
 
 # Dynamically import object_oriented_design_for_impl from the correct path
 import importlib.util
@@ -39,3 +40,28 @@ class Classifier(DataModule): #@save
   def configure_optimizers(self):
     return torch.optim.SGD(self.parameters(),lr=self.lr)
 
+@add_to_class(Classifier) #@save
+def accuracy(self,Y_hat,Y,averaged=True):
+  Y_hat = Y_hat.reshape((-1, Y_hat.shape[-1]))
+  preds = Y_hat.argmax(axis=1).astype(Y.dtype)
+  compare = (preds == Y.reshape(-1).astype(np.float32))
+  return compare.mean() if averaged else compare
+
+@add_to_class(DataModule) #@save
+def get_scratch_params(self):
+  params = []
+  for attr in dir(self):
+    a= getattr(self,attr)
+    if isinstance(a, np.ndarray):
+      params.append(a)
+    if isinstance(a,DataModule):
+      params.extend(a.get_scratch_params()) # type: ignore
+  return params
+
+@add_to_class(DataModule) #@save
+def parameters(self):
+  params = self.collect_params()
+  return params if isinstance(params,gluon.parameter.ParameterDict) and len(
+    params.keys()) else self.get_scratch_params()
+
+  
